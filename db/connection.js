45de1d084eaeabc7877d8b659b9f10e89593d606
Megan Hawkins <megan.hawkins28@gmail.com>
2023-10-24 12:02:07 -0400

    update connection file

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,13 @@
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-dotenv.config()
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 mongoose
-  .set('strictQuery', true)
-  .connect(process.env.DB_URL || "mongodb://localhost:27017/thai", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .set("strictQuery", true)
+  .connect(process.env.DB_URL || "mongodb://localhost:27017/thai")
   .then((instance) => {
     console.log(`Connected on ${instance.connections[0].name}`);
   })
   .catch((err) => console.log(`Got an error see details, ${err}`));
-  
 
-
-export default mongoose.connection
+export default mongoose.connection;
